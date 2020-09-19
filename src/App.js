aef6dc39d32bba2cd038a0cdba039ed20ea6aa52
Carol Lee <CLee@thrillworks.com>
2020-09-19 09:24:35 -0400

    Refactor with hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import SearchBar from './components/SearchBar';
+import VideoDetails from './components/VideoDetails';
+import VideoList from './components/VideoList';
+import useVideos from './hooks/useVideos';
 
-function App() {
+const App = () => {  
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('buildings');
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]); 
+  }, [videos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <SearchBar 
+        onFormSubmit={search}             
+      />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetails 
+              video={selectedVideo} 
+            />  
+          </div>
+          <div className="five wide column">
+            <VideoList 
+
+              // == {(video) => setSelectedVideo(video)}
+              onVideoSelect={setSelectedVideo} 
+              videos={videos}         
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
